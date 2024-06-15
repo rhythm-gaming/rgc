b5f8ff2313adaa035c673dfebcfac7d5973fad4b
Jimin Park <pjm950@gmail.com>
2024-06-15 22:58:14 +0900

    Added tests for reading notes and empty docs.

--- test/chart.js
+++ test/chart.js
@@ -17,6 +17,18 @@ describe("Chart", function() {
         it("should be able to read `chart/sdvx/calibration.rgc`", async function() {
             const chart_src = await readFile("chart/sdvx/calibration.rgc");
             const chart = Chart.parse(chart_src);
+
+            assert.strictEqual(chart.lane_groups.size, 1);
+            
+            const [lane_group] = [...chart.lane_groups.values()];
+            assert.strictEqual(lane_group.id, "bt");
+            assert.strictEqual(lane_group.dim, 0);
+            assert.strictEqual(lane_group.lanes.length, 4);
+
+            for(const lane of lane_group.lanes) {
+                assert.strictEqual(lane.dim, 0);
+                assert.strictEqual(lane.notes.size, 16);
+            }
         });
     });
 });
