e32660598e22bac8f951f6841caee33f0e2dd2a5
Jimin Park <pjm950@gmail.com>
2025-10-03 22:55:48 +0900

    Added more tests for parseChart.

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -8,7 +8,21 @@ describe("parseChart", async function() {
         const chart_data = await readTestData("chart/sdvx/calibration.rgc");
 
         const chart = parseChart(chart_data);
+
         assert.strictEqual(chart.header.version, "0.2.0");
         assert.strictEqual(chart.header.game, "sdvx");
+
+        assert.strictEqual(chart.meta.title, "Calibration");
+        
+        assert.strictEqual(chart.timing.offset, 1000);
+        assert.strictEqual(chart.timing.res, 1);
+
+        for(const [k, v] of Object.entries(chart.chart)) {
+            assert.strictEqual(k, "bt");
+            assert.strictEqual(v.dim, 0);
+            assert.strictEqual(v.lane.length, 4);
+
+            assert.isTrue(v.lane.every((l) => l.length === 16));
+        }
     });
 });
